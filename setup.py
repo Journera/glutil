0e47ece762263a05abe0276bbb3faaacdb27da54
Don Kuntz <dkuntz@journera.com>
2019-06-24 11:46:47 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Journera/glutil'
 EMAIL = 'opensource@journera.com'
 AUTHOR = 'Journera Inc'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
