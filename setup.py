47331dbc8f7aa4e6309b9b1dc90ac7054d19aac0
Don Kuntz <dkuntz@journera.com>
2019-07-17 14:49:25 -0500

    Increment version to 0.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Journera/glutil'
 EMAIL = 'opensource@journera.com'
 AUTHOR = 'Journera Inc'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.3'
+VERSION = '0.2.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
