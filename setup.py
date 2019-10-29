565ea3258b5254142102ca4a0c849bdbb1ea6b11
Don Kuntz <dkuntz@journera.com>
2019-10-29 13:21:23 -0500

    tag 1.0.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Journera/glutil'
 EMAIL = 'opensource@journera.com'
 AUTHOR = 'Journera Inc'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.0'
+VERSION = '1.0.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
